62a3d34cd048fab8fbad3905125592a095a7653c
JangKunil <jangkeya@gmail.com>
2023-04-06 20:31:34 +0900

    Learn how to use @MethodSource of @ParameterizedTest

--- src/test/java/com/github/hotire/springtest/junit5/JUnit5ParameterizedTest.java
+++ src/test/java/com/github/hotire/springtest/junit5/JUnit5ParameterizedTest.java
@@ -5,8 +5,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Month;
 import java.util.EnumSet;
+import java.util.stream.Stream;
+import org.apache.logging.log4j.util.Strings;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class JUnit5ParameterizedTest {
@@ -37,6 +44,40 @@ class JUnit5ParameterizedTest {
     final EnumSet<Month> months =
       EnumSet.of(Month.SEPTEMBER, Month.OCTOBER, Month.NOVEMBER, Month.DECEMBER);
     assertThat(months).contains(month);
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideStringsForIsBlank")
+  void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input, boolean expected) {
+    assertThat(Strings.isBlank(input)).isEqualTo(expected);
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(CustomArgumentProvider.class)
+  void isBlank_ShouldReturnTrueForNullOrBlankStringsArgProvider(String input, boolean expected) {
+    assertThat(Strings.isBlank(input)).isEqualTo(expected);
+  }
+
+  private static Stream<Arguments> provideStringsForIsBlank() {
+    return Stream.of(
+      Arguments.of(null, true),
+      Arguments.of("", true),
+      Arguments.of("  ", true),
+      Arguments.of("not blank", false)
+    );
+  }
+
+  private static class CustomArgumentProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext)
+      throws Exception {
+      return Stream.of(
+        Arguments.of((String) null),
+        Arguments.of(""),
+        Arguments.of("   ")
+      );
+    }
   }
 
 }
