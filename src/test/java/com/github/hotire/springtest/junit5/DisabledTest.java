e0ef7cfd53e54517ec36780af9ad8a876b3ad7cc
JangKunil <jangkeya@gmail.com>
2023-06-18 11:16:44 +0900

    Add DisabledTest

---
+++ src/test/java/com/github/hotire/springtest/junit5/DisabledTest.java
@@ -0,0 +1,15 @@
+package com.github.hotire.springtest.junit5;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
+class DisabledTest {
+
+  @Disabled
+  @Test
+  void ignore() {
+    Assertions.fail();
+  }
+}
